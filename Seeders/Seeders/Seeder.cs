a41c4df47a81c2565184e5c89eb5b3d38927eae9
Dominik <dominikgasior03@gmail.com>
2024-03-04 22:39:25 +0100

    generated solution, project and created first file. Wrote to mini instruction how to I will can to implement

---
+++ Seeders/Seeders/Seeder.cs
@@ -0,0 +1,16 @@
+﻿namespace Seeders;
+
+public static class Seeder
+{
+    // - all data can manipulate with using faker library.
+    
+    // - fetch data from json from solution with using reflection and then generate object with using mapping mechanism.
+    
+    // - fetch data from csv from solution with using reflection and then generate object with using mapping mechanism.
+    
+    // - mechanism for generate random data to database after run on development environment.
+    
+    // - mechanism for generate random data to test database
+    
+    // - also can be used data from other sources like api, external repository, etc. (if our data have a big size)
+}
